35654b9c8ff1c33d372100a547e900fe9bb1380a
easter721 <easter721@gmail.com>
2023-12-23 22:37:34 +0900

    new

---
+++ frontEnd/src/Components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main(props) {
+  return (
+    <div>
+      <h1>로그인 성공 </h1>
+    </div>
+  );
+}
+
+export default Main;
---
+++ frontEnd/src/Pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Main from "../Components/Main";
+
+function MainPage(props) {
+  return (
+    <div>
+      <>
+        <Main></Main>
+      </>
+    </div>
+  );
+}
+
+export default MainPage;
