4a693ce04cdd8ddb37d3141013b3d28c398ae3aa
easter721 <easter721@gmail.com>
2024-02-28 23:52:56 +0900

    feat: chart 만듬

---
+++ frontEnd/src/Components/Notice.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Notice(props) {
+  return (
+    <div>
+      <h1>공지사항</h1>
+    </div>
+  );
+}
+
+export default Notice;
---
+++ frontEnd/src/Pages/NoticePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Notice from "../Components/Notice";
+
+function NoticePage(props) {
+  return (
+    <div>
+      <Notice />
+    </div>
+  );
+}
+
+export default NoticePage;
